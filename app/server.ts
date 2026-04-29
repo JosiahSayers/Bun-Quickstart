33ad2573a125951506f2c600c11d3e4f4ca8cdf5
Josiah Sayers <josiah.sayers15@gmail.com>
2026-04-29 17:33:27 -0400

    setup better-auth in express

--- app/server.ts
+++ app/server.ts
@@ -3,9 +3,14 @@ import { stashRequestMetadata } from "$/middleware/stash-request-meta";
 import { healthRouter } from "$/routers/health";
 import { attachLogger } from "$/middleware/attach-logger";
 import { requestLogger } from "$/middleware/request-logger";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "$/utils/auth";
 
 export const app = express();
 app.use(stashRequestMetadata, attachLogger, requestLogger);
+app.all("/api/auth/*", toNodeHandler(auth));
 app.disable("x-powered-by");
+
+app.use(express.json());
 
 app.use(healthRouter);
